a0ef814a6228a9ef70fd4c0a1820ac8f5fd3f101
Buddhima Fernando <harun.fernando.hf@gmail.com>
2024-06-14 21:11:30 +0530

    adding the app bar for the home

--- lib/srceens/home.dart
+++ lib/srceens/home.dart
@@ -5,6 +5,28 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(140.0),
+        child: AppBar(
+          backgroundColor: Theme.of(context).primaryColor,
+          leading: Padding(
+            padding: const EdgeInsets.only(top: 10,left: 10.0),
+            child: ImageIcon(
+              AssetImage("assets/images/dp.jpg"),
+              size: 100,
+              color: Theme.of(context).cardColor,
+            ),
+          ),
+          title: Text(
+            "Money Tracking",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+              color: Theme.of(context).cardColor,)
+            ),
+        ),
+      ),
+    );
   }
 }
